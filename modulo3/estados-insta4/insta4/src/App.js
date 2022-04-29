23afd489865cddfeaf5d52a8b96400e0267d60a5
Gabriel Oliveira <gabrieloliveira-lg@hotmail.com>
2022-04-29 00:01:59 -0300

    Exercícios Incompleto!

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -10,29 +10,106 @@ const MainContainer = styled.div`
 `
 
 class App extends React.Component {
+
+  state = {
+    usuarios: [
+      {
+        nomeUsuario: 'Filipe Vidal',
+        fotoUsuario: 'img/png.opdownload',
+        fotoPost: 'img/img1.jpg'
+      },
+
+      {
+        nomeUsuario: 'Gabriel Oliveira',
+        fotoUsuario: 'img/Gabriel.png',
+        fotoPost: 'img/img2.jpg'
+      },
+
+      {
+        nomeUsuario: 'Barbara Claudino',
+        fotoUsuario: 'img/Bárbara.png',
+        fotoPost: 'img/img3.jpg'
+      }
+    ],
+
+    valorInputNomeUsuario: "",
+    valorInputFotoUsuario: "",
+    valorInputFotoPost: ""
+  }
+
+  adicionaPost = () => {
+    const novoPost = {
+      nomeUsuario: this.state.valorInputNomeUsuario,
+      fotoUsuario: this.state.valorInputFotoUsuario,
+      fotoPost: this.state.valorInputFotoPost
+    };
+
+    const novosPosts = [...this.state.usuarios, novoPost];
+
+    this.setState({
+      pessoas: novosPosts,
+      valorInputNomeUsuario: "",
+      valorInputFotoUsuario: "",
+      valorInputFotoPost: ""
+    });
+  };
+
+  onChangeInputNomeUsuario = (event) => {
+    this.setState({ valorInputNomeUsuario: event.target.value });
+  };
+
+  onChangeInputFotoUsuario = (event) => {
+    this.setState({ valorInputFotoUsuario: event.target.value });
+  };
+
+  onChangeInputFotoPost = (event) => {
+    this.setState({ valorInputFotoPost: event.target.value });
+  };
+
+
   render() {
+    const listaDeUsuarios = this.state.usuarios.map((usuarios) => {
+      return (
+        <Post
+          nomeUsuario={usuarios.nomeUsuario}
+          fotoUsuario={usuarios.fotoUsuario}
+          fotoPost={usuarios.fotoPost}
+        />
+      )
+    })
     return (
-      <MainContainer>
-        <Post
-          nomeUsuario={'Filipe Vidal'}
-          fotoUsuario={'img/png.opdownload'}
-          fotoPost={'img/img1.jpg'}
-        />
 
-        <Post
-          nomeUsuario={'Gabriel Oliveira'}
-          fotoUsuario={'img/Gabriel.png'}
-          fotoPost={'img/img2.jpg'}
-        />
+      <div>
+        <div>
+          <h1>Adicionar Post</h1>
+          <div>
+            <input
+              value={this.state.valorInputNomeUsuario}
+              onChange={this.onChangeInputNomeUsuario}
+              placeholder={"Nome do Usuário"}
+            />
+            <input
+              value={this.state.valorInputFotoUsuario}
+              onChange={this.onChangeInputFotoUsuario}
+              placeholder={"Foto do Usuário"}
+            />
+            <input
+              value={this.state.valorInputFotoPost}
+              onChange={this.onChangeInputFotoPost}
+              placeholder={"Foto Post"}
+            />
+            <button onClick={this.novosPosts}>Adicionar</button>
+          </div>
+        </div>
 
-        <Post
-          nomeUsuario={'Barbara Claudino'}
-          fotoUsuario={'img/Bárbara.png'}
-          fotoPost={'img/img3.jpg'}
-        />
 
 
-      </MainContainer>
+
+        <MainContainer>
+          {listaDeUsuarios}
+        </MainContainer>
+
+      </div>
     );
   }
 }
