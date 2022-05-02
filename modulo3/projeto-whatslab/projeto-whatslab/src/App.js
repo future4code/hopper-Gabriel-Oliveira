79b64d335af672aa6b80e451235f022314a53985
Gabriel Oliveira <gabrieloliveira-lg@hotmail.com>
2022-05-01 22:46:51 -0300

    Projeto Incompleto

--- modulo3/projeto-whatslab/projeto-whatslab/src/App.js
+++ modulo3/projeto-whatslab/projeto-whatslab/src/App.js
@@ -1,25 +1,147 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import styled from 'styled-components'
+import './App.css'
+import ChatList from "./componentes/ChatList";
 
-function App() {
+
+const AreaContatos = styled.div`
+  display: flex;
+  height: 100vh;
+  background-color: #EDEDED;
+`
+
+const BarraLateral = styled.div`
+  width: 35%;
+  max-width: 415px;
+  display: flex;
+  flex-direction: column;
+  border-right: 1px solid #DDD;
+`
+const CabecalhoContatos = styled.header`
+  height: 60px;
+  display: flex;
+  justify-content: space-betWeen;
+  align-items: center;
+  padding: 10px 15px;
+`
+
+const Botoes = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`
+
+const Pesquisa = styled.div`
+  background-color: #F6F6F6;
+  border-bottom: 1px solid #EEE;
+  padding: 5px 15px;
+`
+
+const InputBusca = styled.div`
+  background-color: #FFF;
+  height: 40px;
+  border-radius: 20px;
+  display: flex;
+  align-items: center;
+  padding: 0 10px;
+
+ input{
+   flex: 1;
+   border: 0;
+   outline: 0;
+   background-color: transparent;
+   margin-left: 10px;
+ }
+`
+
+const Chat = styled.div`
+  flex: 1;
+  background-color: #FFF;
+  overflow-y: auto;
+
+  &::-webkit-scrollbar {
+    width: 6px;
+    height: 6px;
+  }
+
+  &::-webkit-scrollbar-thumb{
+    background-color: rgba(0,0,0,0.2);
+  }
+`
+
+const HeaderAvatar = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 20px;
+  cursor: pointer;
+`
+
+const HeaderBotao = styled.div`
+  display: flex;
+`
+
+
+export default () => {
+
+  const [listMensagem, setChatList] = useState([{}, {}, {}, {},{}, {}, {}, {},{}, {}, {}, {}])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+    <AreaContatos>
+
+      <BarraLateral>
+
+        <CabecalhoContatos>
+
+          <HeaderAvatar src="https://bombyxplm.com/wp-content/uploads/2021/01/421-4213053_default-avatar-icon-hd-png-download.png" alt="Avatar" />
+
+          <HeaderBotao>
+            <Botoes>
+              <img src="" alt="Status" />
+            </Botoes>
+
+            <Botoes>
+              <img src="" alt="NewMensage" />
+            </Botoes>
+
+            <Botoes>
+              <img src="" alt="TrêsPontos" />
+            </Botoes>
+          </HeaderBotao>
+
+        </CabecalhoContatos>
+
+        <Pesquisa>
+          <InputBusca>
+            <img src="/" alt="Icone de Pesquisa" />
+            <input type="search" placeholder="Procurar ou começar uma nova conversa." />
+          </InputBusca>
+        </Pesquisa>
+
+        <Chat>
+          {listMensagem.map((item, key) => (
+            <ChatList
+              key={key}
+            />
+          ))}
+        </Chat>
+
+
+      </BarraLateral>
+
+      <div>
+        ...
+      </div>
+
+
+    </AreaContatos>
+
+
+  )
+
+
+}
---
+++ modulo3/projeto-whatslab/projeto-whatslab/src/componentes/ChatList.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ItemLista = styled.div`
+    display: flex;
+    cursor: pointer;
+    align-items: center;
+    height: 70px;
+
+    &:hover{
+       background-color: #F5F5F5;
+    }
+`
+
+const ImgAvatar = styled.img`
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    margin-left: 15px;
+    margin-right: 10px;
+`
+
+const Linhas = styled.div`
+    flex: 1;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    border-bottom: 1px solid #EEE;
+    padding-right: 15px;
+    margin left: 15px;
+
+    flex-wrap: wrap;
+    min-width: 0;
+`
+
+const Linha = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+`
+
+const Nome = styled.div`
+    font-size: 17px;
+    color: #000;
+`
+
+const Data = styled.div`
+    font-size: 12px;
+    color: #999;
+`
+
+const ProximaMensagem = styled.div`
+    font-size: 14px;
+    color: #999;
+    display: flex;
+    width: 100%;
+
+p{
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    margin:0;
+
+    }
+`
+
+
+
+
+
+
+export default () => {
+    return (
+
+        <ItemLista>
+            <ImgAvatar src='https://bombyxplm.com/wp-content/uploads/2021/01/421-4213053_default-avatar-icon-hd-png-download.png' alt='' />
+            <Linhas>
+                <Linha>
+                    <Nome>Gabriel Oliveira</Nome>
+                    <Data>4:54</Data>
+                </Linha>
+
+                <Linha>
+                    <ProximaMensagem>
+                        <p> Olá, como você esta? Olá, como você esta? Olá, como você esta? </p>
+                    </ProximaMensagem>
+                </Linha>
+            </Linhas>
+        </ItemLista>
+
+    )
+}
