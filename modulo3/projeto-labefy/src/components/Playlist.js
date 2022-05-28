0837554ac575ebbbb43c93a53c308556ab6f66c5
Gabriel Oliveira <gabrieloliveira-lg@hotmail.com>
2022-05-28 03:03:45 -0300

    Algumas estilizações

--- modulo3/projeto-labefy/src/components/Playlist.js
+++ modulo3/projeto-labefy/src/components/Playlist.js
@@ -12,16 +12,13 @@ export default class CreatePlaylist extends React.Component {
 
 
     state = {
+        namePlaylist: "",
         playlistsCriadas: [],
-
-
         musicasPlaylists: [],
-
-
-
-        inputNamePlaylist: "",
+        inputNameMusica: "",
         inputNameArtistaOuBanda: "",
-        inputUrlMusica: ""
+        inputUrlMusica: "",
+        idPlaylist: ""
 
     }
 
@@ -111,11 +108,84 @@ export default class CreatePlaylist extends React.Component {
 
     // ADICIONAR MUSICA A PLAYLIST
 
-    addMusicasPlaylist = () => {
+    addMusicas = () => {
+        const body = {
+            name: this.state.inputNameMusica,
+            artist: this.state.inputNameArtistaOuBanda,
+            url: this.state.inputUrlMusica
+        }
+        axios.post(
+            `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${this.state.idPlaylist}/tracks`,
+            body,
+            {
+                headers: {
+                    Authorization: "gabriel-oliveira-hopper"
+                }
+            }
+        )
+            .then(() => {
+                this.setState({ inputNameMusica: "", inputNameArtistaOuBanda: "", inputUrlMusica: "" })
+                alert('Música adicionada com sucesso!')
+                this.selecionaPlaylist(this.state.idPlaylist)
+            })
+            .catch(() => {
+                alert('Ops, Erro ao adicionar a Música!')
+            })
+    }
 
+    selecionaPlaylist = id => {
+        axios
+            .get(
+                `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${id}/tracks`,
+                {
+                    headers: {
+                        Authorization: "gabriel-oliveira-hopper"
+                    }
+                }
+            )
+            .then(response => {
+                this.setState({ musicasPlaylists: response.data.result.list, idPlaylist: id })
+            })
+            .catch(err => {
+                alert(err.response.data)
+            })
+    }
+
+    onChangeMusicas = event => {
+        this.setState({ inputNameMusica: event.target.value })
+    }
+
+    onChangeArtistas = event => {
+        this.setState({ inputNameArtistaOuBanda: event.target.value })
+    }
+
+    onChangeUrl = event => {
+        this.setState({ inputUrlMusica: event.target.value })
     }
 
 
+
+    // DELETAR MÚSICA DA PLAYLIST
+
+
+    deletaMusicas = (idPlay, idMusic) => {
+        axios
+            .delete(
+                `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${idPlay}/tracks/${idMusic}`,
+                {
+                    headers: {
+                        Authorization: "gabriel-oliveira-hopper"
+                    }
+                }
+            )
+            .then(() => {
+                this.selecionaPlaylist(this.state.idPlaylist)
+                alert('Música excluída com sucesso!')
+            })
+            .catch(() => {
+                alert('Ops, aconteceu um erro ao excluir Música!')
+            })
+    }
 
 
 
@@ -136,32 +206,79 @@ export default class CreatePlaylist extends React.Component {
             <div className="container-principal">
 
                 <div className="area-header">
-                    <div>
+                    <div className="itens-header">
                         <img src="img/spotify.png" ></img>
                         <h1>Labefy</h1>
                     </div>
                 </div>
 
+
+
                 <div className="areaAddPlaylist">
-                    <h3>CRIAR PLAYLIST:</h3>
-                    <input
-                        placeholder="Nome da Playlist"
-                        value={this.state.namePlaylist}
-                        onChange={this.onchangeNamePlaylist}
-                    />
-                    <button onClick={this.CriaPlaylist}>Criar Playlist</button>
+                    <div className="itemAddPlaylist">
+                        <h3>CRIAR PLAYLIST:</h3>
+                        <div>
+                            <input
+                                placeholder="Nome da Playlist"
+                                value={this.state.namePlaylist}
+                                onChange={this.onchangeNamePlaylist}
+                            />
+                            <img onClick={this.CriaPlaylist} src="img/lista-de-reproducao.png"></img>
+                        </div>
+                    </div>
                 </div>
 
-                <h2>LISTA DE PLAYLISTS:</h2>
-                <div>
-                    {renderedPlaylists}
+
+
+                <div className="areaAddTracksPlaylist">
+                    <h3>ADICIONE SUAS MÚSICAS:</h3>
+                    <div>
+                        <input
+                            placeholder="Insira o nome da Música"
+                            value={this.state.inputNameMusica}
+                            onChange={this.onChangeMusicas}
+                            type="text"
+                        />
+                        <input
+                            placeholder="Insira o nome do Artista/Banda"
+                            value={this.state.inputNameArtistaOuBanda}
+                            onChange={this.onChangeArtistas}
+                            type="text"
+                        />
+                        <input
+                            placeholder="Insira o link da Música"
+                            value={this.state.inputUrlMusica}
+                            onChange={this.onChangeUrl}
+                            type="url"
+                        />
+                    </div>
+                    <img src="img/adicionar.png" onClick={this.addMusicas}></img>
                 </div>
+
+
+                <div className="areaPlaylists">
+                    <h2>LISTA DE PLAYLISTS:</h2>
+                    <div>
+                        {renderedPlaylists}
+                    </div>
+                </div>
+
+
+
+
+
                 <h2>LISTA DE MÚSICAS:</h2>
                 <div>
                     {renderedTracksPlaylists}
                 </div>
 
 
+
+                <div className="areaFooter">
+                    <div>
+                        <h2> © 2022 Labefy | Gabriel Rosa de Oliveira - Turma Hopper Labenu</h2>
+                    </div>
+                </div>
 
 
             </div >
