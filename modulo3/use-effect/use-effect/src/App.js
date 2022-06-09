9ff2167829db05020cd30a98be8de1a5599be373
Gabriel Oliveira <gabrieloliveira-lg@hotmail.com>
2022-06-08 21:27:53 -0300

    Alterações ícones

--- modulo3/use-effect/use-effect/src/App.js
+++ modulo3/use-effect/use-effect/src/App.js
@@ -5,10 +5,10 @@ import PokeCard from "./Components/PokeCard/PokeCard";
 
 
 
-export default function App (){
+export default function App() {
   const [pokeList, setPokeList] = useState([])
   const [pokeName, setPokeName] = useState("")
-  
+
   useEffect(() => {
     axios
       .get("https://pokeapi.co/api/v2/pokemon/?limit=151")
@@ -23,20 +23,20 @@ export default function App (){
   const changePokeName = (event) => {
     setPokeName(event.target.value);
   };
-  
-  return(
+
+  return (
     <div className="App">
-        <select onChange={changePokeName}>
-          <option value={""}>Nenhum</option>
-          {pokeList.map((pokemon) => {
-            return (
-              <option key={pokemon.name} value={pokemon.name}>
-                {pokemon.name}
-              </option>
-            );
-          })}
-        </select>
-        {pokeName && <PokeCard pokemon={pokeName} />}
+      <select onChange={changePokeName}>
+        <option value={""}>Nenhum</option>
+        {pokeList.map((pokemon) => {
+          return (
+            <option key={pokemon.name} value={pokemon.name}>
+              {pokemon.name}
+            </option>
+          );
+        })}
+      </select>
+      {pokeName && <PokeCard pokemon={pokeName} />}
     </div>
   )
 }
--- modulo3/use-effect/use-effect/src/Components/PokeCard/PokeCard.js
+++ modulo3/use-effect/use-effect/src/Components/PokeCard/PokeCard.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "../../App.css";
 
 
 export default function PokeCard(props){
@@ -35,9 +36,9 @@ export default function PokeCard(props){
 
     return(
         <div>
-        <p>{pokemon.name}</p>
-        <p>{pokemon.weight} Kg</p>
-        {pokemon.types && <p>{pokemon.types[0].type.name}</p>}
+        <p className="itens"><img className="img-ident" src="img/pokemon.png"></img>{pokemon.name}</p>
+        <p className="itens"><img  className="img-ident" src="img/peso-pesado.png"></img>{pokemon.weight} Kg</p>
+        {pokemon.types && <p className="itens"><img  className="img-ident" src="img/estrela.png"></img>{pokemon.types[0].type.name}</p>}
         {pokemon.sprites && (
           <img src={pokemon.sprites.front_default} alt={pokemon.name} />
         )}
