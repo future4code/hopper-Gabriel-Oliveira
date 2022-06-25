94f38c5c34d3850601a7a2aedf3d86abf322b999
Gabriel Oliveira <gabrieloliveira-lg@hotmail.com>
2022-06-24 21:24:11 -0300

    Projeto incompleto

--- modulo4/projeto-labex/projeto-labex/src/pages/AdminHomePage.js
+++ modulo4/projeto-labex/projeto-labex/src/pages/AdminHomePage.js
@@ -5,12 +5,32 @@ import "../styles.css";
 
 const AdminHomePage = () => {
   const navigate = useNavigate();
-  const [tripsAdm, setTripsAdm] = useState();
-
+  const [admTrips, setAdmTrips] = useState([]);
 
   useEffect(() => {
     getTripsAdm();
   }, [navigate]);
+
+
+  
+  
+  const deletarTrip = (id) => {
+    const headers = {
+      headers: {
+          auth: localStorage.getItem("token")
+      },
+    };
+    axios.
+    delete( `https://us-central1-labenu-apis.cloudfunctions.net/labeX/gabriel-oliveira-hopper/trips/${id}` , headers)
+    .then((res) => {
+      alert("Viagem excluída com sucesso!")
+      getTripsAdm();
+    })
+    .catch((err) => {
+      alert(err.data.message)
+    })
+
+  }
 
   const getTripsAdm = () => {
     axios
@@ -18,7 +38,7 @@ const AdminHomePage = () => {
         "https://us-central1-labenu-apis.cloudfunctions.net/labeX/gabriel-oliveira-hopper/trips"
       )
       .then((response) => {
-        setTripsAdm(response.data.trips);
+        setAdmTrips(response.data.trips);
         console.log(response.data.trips);
       })
       .catch((err) => {
@@ -26,17 +46,12 @@ const AdminHomePage = () => {
       });
   };
 
-
-
-
-
   return (
-    <div className="container-listTrips"  >
+    <div className="container-listTrips">
       <div className="iconeAndtitulo-trips">
         <h1 className="titulo-trips">ÁREA ADMINISTRADOR</h1>
         <img className="icone-trips" src="/IMG/definicoes.png" alt="ícone" />
       </div>
-
       <div className="buttons-listTrips">
         <button className="btn-logout" onClick={() => navigate("/")}>
           LOGOUT
@@ -51,22 +66,37 @@ const AdminHomePage = () => {
           VOLTAR
         </button>
       </div>
-
-      {tripsAdm.map((tripAdm) => {
+      {admTrips.map((trip) => {
         return (
           <div className="list-trips">
-          <div className="blog_postAdm">
-            <div className="container_copy_areaAdm">
-              <h1 className="text-titulo-trips">{tripAdm.name}</h1>
-              <h2 className="text-planeta-list">Planeta: {tripAdm.planet}</h2>
-              <h3 className="text-dateAndDuration-trips"> Data: {tripAdm.date}</h3>
-              <h3 className="text-dateAndDuration-trips"> Duração: {tripAdm.durationInDays} Dias </h3>
+            <div className="blog_postAdm">
+              <div className="container_copy_areaAdm">
+                <div>
+                  <h1 className="text-titulo-trips">{trip.name}</h1>
+                  <h2 className="text-planeta-list">Planeta: {trip.planet}</h2>
+                  <h3 className="text-dateAndDuration-trips">
+                    {" "}
+                    Data: {trip.date}
+                  </h3>
+                  <h3 className="text-dateAndDuration-trips">
+                    {" "}
+                    Duração: {trip.durationInDays} Dias{" "}
+                  </h3>
+                </div>
+                <div>
+                  <img
+                    onClick={() => deletarTrip(trip.id)}
+                    className="img-excluir-adm"
+                    src="/IMG/excluir.png"
+                    alt="Excluir"
+                  />
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        
-        )})};
-
+        );
+      })}
+      ;
     </div>
   );
 };
