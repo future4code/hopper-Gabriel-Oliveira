2d3fb8039641e0d7270849c48c03a0c3a6024a8e
Gabriel Oliveira <gabrieloliveira-lg@hotmail.com>
2022-06-27 21:27:40 -0300

    Ainda faltam datlhes e candidatos

--- modulo4/projeto-labex/projeto-labex/src/pages/ApplicationFormPage.js
+++ modulo4/projeto-labex/projeto-labex/src/pages/ApplicationFormPage.js
@@ -1,21 +1,138 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles.css";
 
 const ApplicationFormPage = () => {
   const navigate = useNavigate();
+  const [name, setName] = useState();
+  const [age, setAge] = useState();
+  const [applicationText, setApplicationText] = useState();
+  const [profession, setProfession] = useState();
+  const [country, setCountry] = useState();
+  const [tripsAPF, setTripsAPF] = useState([]);
+
+  useEffect(() => {
+    getTripsAFP();
+  }, []);
+
+  const onChangeName = (event) => {
+    setName(event.target.value);
+  };
+
+  const onChangeAge = (event) => {
+    setAge(event.target.value);
+  };
+
+  const onChangeApplicationText = (event) => {
+    setApplicationText(event.target.value);
+  };
+
+  const onChangeProfession = (event) => {
+    setProfession(event.target.value);
+  };
+
+  const onChangeCountry = (event) => {
+    setCountry(event.target.value);
+  };
+
+  const getTripsAFP = () => {
+    axios
+      .get(
+        "https://us-central1-labenu-apis.cloudfunctions.net/labeX/gabriel-oliveira-hopper/trips"
+      )
+      .then((res) => {
+        setTripsAPF(res.data.trips);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const onSubmitApplicationForm = (event, id) => {
+    event.preventDefault();
+
+    const body = {
+      name: name,
+      age: age,
+      applicationText: applicationText,
+      profession: profession,
+      country: country
+    };
+
+    axios
+      .post(
+        `https://us-central1-labenu-apis.cloudfunctions.net/labeX/gabriel-oliveira-hopper/trips/${id}/apply`,
+        body
+      )
+      .then((res) => {
+        alert(res.data.message);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setName("");
+        setAge("");
+        setApplicationText("");
+        setProfession("");
+        setCountry("");
+      });
+  };
+
   return (
-    <div>
-      <h1>APPLICATION FORM PAGE</h1>
-      <button
-        className="btn-inscrever"
-        onClick={() => navigate("/trips/application")}
-      >
-        INSCREVER-SE
-      </button>
-      <button className="btn-voltar" onClick={() => navigate("/trips/list")}>
-        VOLTAR
-      </button>
+    <div className="container-loginPage">
+      <div className="box">
+        <form onSubmit={onSubmitApplicationForm}>
+          <div className="iconeAndTitulo-login">
+            <img
+              src="/IMG/logo.png"
+              className="img-icone-login"
+              alt="icone-logo"
+            />
+          </div>
+          <div className="select-planetas">
+            <select required>
+              <option value="">Selecione a viagem que gostaria de se inscrever.</option>
+              {tripsAPF &&
+                tripsAPF.map((trip) => (
+                  <option key={trip.id} value={tripsAPF}>
+                    {trip.name}
+                  </option>
+                ))}
+            </select>
+            <label>Viagem:</label>
+          </div>
+          <div className="input-container">
+            <input onChange={onChangeName} value={name} type="name" required />
+            <label>Nome:</label>
+          </div>
+          <div className="input-container">
+            <input onChange={onChangeAge} value={age} type="number" required />
+            <label>Idade:</label>
+          </div>
+          <div className="input-container">
+            <input onChange={onChangeApplicationText} value={applicationText} type="text" required />
+            <label>Descrição:</label>
+          </div>
+          <div className="input-container">
+            <input onChange={onChangeProfession} value={profession} type="text" required />
+            <label>Profissão:</label>
+          </div>
+          <div className="input-container">
+            <input onChange={onChangeCountry} value={country} type="text" required />
+            <label>País:</label>
+          </div>
+          <div className="btns-login">
+            <button className="btn-criar">CRIAR</button>
+            <button
+              className="btn-voltar"
+              onClick={() => navigate("/admin/trips/list")}
+            >
+              VOLTAR
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
--- modulo4/projeto-labex/projeto-labex/src/pages/CreateTripPage.js
+++ modulo4/projeto-labex/projeto-labex/src/pages/CreateTripPage.js
@@ -1,19 +1,141 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles.css";
 
 const CreateTripPage = () => {
   const navigate = useNavigate();
+  const [name, setName] = useState();
+  const [planet, setPlanet] = useState();
+  const [date, setDate] = useState();
+  const [description, setDescription] = useState();
+  const [durationInDays, setDurationInDays] = useState();
+
+  const onChangeName = (event) => {
+    setName(event.target.value);
+  };
+
+  const onChangePlanet = (event) => {
+    setPlanet(event.target.value);
+  };
+
+  const onChangeDate = (event) => {
+    setDate(event.target.value);
+  };
+
+  const onChangeDescription = (event) => {
+    setDescription(event.target.value);
+  };
+
+  const onChangeDurationInDays = (event) => {
+    setDurationInDays(event.target.value);
+  };
+
+  const submitCreateTrips = (event) => {
+    event.preventDefault();
+
+    const headers = {
+      headers: {
+        auth: localStorage.getItem("token")
+      }
+    };
+
+    const body = {
+      name: name,
+      planet: planet,
+      date: date,
+      description: description,
+      durationInDays: durationInDays
+    };
+
+    axios
+      .post(
+        "https://us-central1-labenu-apis.cloudfunctions.net/labeX/gabriel-oliveira-hopper/trips",
+        body,
+        headers
+      )
+      .then((res) => {
+        console.log(res.data.trips);
+        alert("Viagem criada com sucesso.");
+      })
+      .catch((err) => {
+        alert(err.message);
+      })
+      .finally(() => {
+        setName("");
+        setPlanet("");
+        setDate("");
+        setDescription("");
+        setDurationInDays("");
+      });
+  };
+
   return (
-    <div>
-      <h1>CREATE TRIP PAGE</h1>
-      <button className="btn-criar">CRIAR</button>
-      <button
-        className="btn-voltar"
-        onClick={() => navigate("/admin/trips/list")}
-      >
-        VOLTAR
-      </button>
+    <div className="container-loginPage">
+      <div className="box">
+        <form onSubmit={submitCreateTrips}>
+          <div className="iconeAndTitulo-login">
+            <img
+              src="/IMG/logo.png"
+              className="img-icone-login"
+              alt="icone-logo"
+            />
+          </div>
+          <div className="input-container">
+            <input type="name" onChange={onChangeName} value={name} required />
+            <label>Nome da Viagem:</label>
+          </div>
+          <div className="select-planetas">
+            <select onChange={onChangePlanet} value={planet} required>
+              <option value=""> Selecione o Destino </option>
+              <option value="Mercúrio">Mercúrio</option>
+              <option value="Vênus">Vênus</option>
+              <option value="Terra">Terra</option>
+              <option value="Marte">Marte</option>
+              <option value="Júpiter">Júpiter</option>
+              <option value="Saturno">Saturno</option>
+              <option value="Urano">Urano</option>
+              <option value="Netuno">Netuno</option>
+              <option value="Plutão">Plutão</option>
+              <option value="Ceres">Ceres</option>
+              <option value="Éris">Éris</option>
+              <option value="Makemake">Makemake</option>
+              <option value="Haumea">Haumea</option>
+            </select>
+            <label>Planeta:</label>
+          </div>
+          <div className="input-container">
+            <input onChange={onChangeDate} value={date} type="date" required />
+            <label>Data:</label>
+          </div>
+          <div className="input-container">
+            <input
+              onChange={onChangeDescription}
+              value={description}
+              type="text"
+              required
+            />
+            <label>Descrição:</label>
+          </div>
+          <div className="input-container">
+            <input
+              onChange={onChangeDurationInDays}
+              value={durationInDays}
+              type="number"
+              required
+            />
+            <label>Duração em dias:</label>
+          </div>
+          <div className="btns-login">
+            <button className="btn-criar">CRIAR</button>
+            <button
+              className="btn-voltar"
+              onClick={() => navigate("/admin/trips/list")}
+            >
+              VOLTAR
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
