b165f935068c1ea27e2016a7cebf0c71a66881dd
Gabriel Oliveira <gabrieloliveira-lg@hotmail.com>
2022-08-28 20:10:25 -0300

    Exercícios Fluxo de Dados

---
+++ modulo5/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,24 @@
+import { Produtos } from "./types"
+
+export const listProdutos: Produtos[] = [
+    {
+      id: "1",
+      name: "Processador Intel Core i3 10100F 3.60GHz",
+      price: 499.00
+    },
+    {
+      id: "2",
+      name: "Processador Intel Core i5 10400F 2.90GHz",
+      price: 839.90
+    },
+    {
+      id: "3",
+      name: "Processador Intel Core i7 10700F, 2.90GHz",
+      price: 1859.00
+    },
+    {
+      id: "4",
+      name: "Processador Intel Core i9 10900F, 2.80GHz",
+      price: 2659.00
+    }
+  ]
