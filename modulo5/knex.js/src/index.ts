614937bc8139ad9ccd23054a4d0ecf2492638e96
Gabriel Oliveira <gabrieloliveira-lg@hotmail.com>
2022-09-06 23:50:11 -0300

    Exercícios Knex.Js

---
+++ modulo5/knex.js/src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(process.env.PORT || 3003, () => {
+  console.log(`Servidor rodando na porta ${process.env.PORT || 3003}`)
+});
